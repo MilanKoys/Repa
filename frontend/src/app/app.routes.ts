47b37543e6ba5ae2a36851b6fb0a09181f2a6825
Milan Koyš <milankoys@tuta.com>
2025-04-12 12:26:53 +0200

    Login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
---
+++ frontend/src/app/views/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../../components';
+
+@Component({
+  selector: 'app-login',
+  imports: [InputComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
